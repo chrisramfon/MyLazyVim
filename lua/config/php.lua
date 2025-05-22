8a1df99c9615579a5131c906fb4db4843f5e8f93
Christian Ramos <christian.ramos@milenio.com>
2025-05-22 15:41:20 +0000

    [UPDATE] This commit adds a new configuration for php debugging.

---
+++ lua/config/php.lua
@@ -0,0 +1,20 @@
+local dap = require("dap")
+
+dap.adapters.php = {
+  type = "executable",
+  command = "node",
+  args = { "/root/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js" },
+}
+
+dap.configurations.php = {
+  {
+    type = "php",
+    request = "launch",
+    name = "Listen for Xdebug",
+    port = 9001,  -- Default port for Xdebug 3.x
+    pathMappings = {
+      ["/srv/camus/api"] = "${workspaceFolder}",  -- Adjust this to your server's document root and local project path
+    },
+  },
+}
+
---
+++ lua/plugins/dap.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    name = "dap",
+    lazy = true,
+    config = function() end,
+    keys = {
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Start/Continue Debugging" },
+      { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+      { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate Debugging" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<leader>dw", function() require("dap").repl.open() end, desc = "Inspect the State" },
+      { "<leader>du", function() require("dapui").toggle() end, desc = "Toggle UI" },
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" }, -- Ensures correct load order
+    config = function()
+      require("dapui").setup()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+}
+
